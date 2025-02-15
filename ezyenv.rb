e28631ee83a899ac4f0fe757076b16a094d60c55
broisnischal <neeswebservice@gmail.com>
2025-02-15 15:17:23 +0545

    feat:publish

--- ezyenv.rb
+++ ezyenv.rb
@@ -1,8 +1,7 @@
 class Ezyenv < Formula
   desc "Ezyenv - CLI for managing environments"
   homepage "https://github.com/broisnischal/ezyenv"
-  version "1.0.0"
-  url "https://github.com/broisnischal/ezyenv/releases/download/v1.4/ezyenv"
+  version "1.0.1"
   sha256 "b99fa4da67400f7b73c55fe1d6b791b33f86b687a54dbc17a287bfd12e4046f0"
 
   def install
